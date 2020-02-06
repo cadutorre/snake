2812e4dc01bc0bf4a84e892172d9ece99251ed2b
Carlos Eduardo Torre <cadutorre@gmail.com>
2020-02-06 01:10:25 -0300

    Organizando codigo e separando responsabilidades

---
+++ src/render-screen.js
@@ -0,0 +1,20 @@
+export default function renderScreen(screen, game, requestAnimationFrame) {
+  const context = screen.getContext("2d");
+  context.clearRect(0, 0, screen.width, screen.height);
+
+  for (const playerId in game.state.players) {
+    const palyer = game.state.players[playerId];
+    context.fillStyle = "black";
+    context.fillRect(palyer.x, palyer.y, 1, 1);
+  }
+
+  for (const fruitId in game.state.fruits) {
+    const fruit = game.state.fruits[fruitId];
+    context.fillStyle = "green";
+    context.fillRect(fruit.x, fruit.y, 1, 1);
+  }
+
+  requestAnimationFrame(() => {
+    renderScreen(screen, game, requestAnimationFrame);
+  });
+}
